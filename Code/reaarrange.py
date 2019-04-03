3827c27e655483880f7abf2e1cbd99ea0a2184e1
Drake Vorndran <DrakeVorndranOffical@gmail.com>
2019-04-03 13:14:18 -0700

    added reverse and anagram generator as well as changed the scramble to Fisher-Yates

--- Code/reaarrange.py
+++ Code/reaarrange.py
@@ -2,12 +2,26 @@ import sys
 import random
 
 def reaarrange(words):
-  for x in range(random.randint(1,len(words)*2)):
-    pos1 = random.randint(0,len(words)-1)
-    pos2 = random.randint(0,len(words)-1)
-    words[pos1], words[pos2] = words[pos2], words[pos1]
+  for x in reversed(range(1,len(words))):
+    pos = random.randint(0,x-1)
+    words[x], words[pos] = words[pos], words[x]
   return(words)
+
+def reverseString(string, seperator=""):
+  if(seperator == ""):
+    return_string = list(string)
+    
+  else:
+    return_string = string.split(seperator)
+  return_string.reverse()
+  return return_string
+
+def anagram(word):
+  return "".join(reaarrange(list(word)))
+
 
 if __name__ == '__main__':
   l = sys.argv[1:]
   print(reaarrange(l))
+  # print(reverseString("hello my name is bob", " "))
+  # print(anagram("anagram"))
